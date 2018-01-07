090c443a666c9f66624bd77ba49bfe8fa9b61d58
Druthi Polisetty <polisettydruthi@gmail.com>
2018-01-07 13:26:49 +0530

    Set up routing. Login Form created

--- rwa_portal_app/src/index.js
+++ rwa_portal_app/src/index.js
@@ -2,7 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import LoginForm from './components/LoginForm';
+import RwaApp from './components/RwaApp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//let store = createStore(info)
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path ="/" component={App}>
+      <IndexRoute component={LoginForm}></IndexRoute>
+      <Route path="/RwaApp" component={RwaApp}>
+      </Route>
+    </Route>
+  </Router>
+)
+ReactDOM.render(router, document.getElementById('root'));
+
+
+
+
